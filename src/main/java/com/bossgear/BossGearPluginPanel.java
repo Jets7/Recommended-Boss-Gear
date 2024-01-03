0ec8d94859e11416cb7cd78420b4127cb67f65cc
Nic-Renfrow <s535876@nwmissouri.edu>
2024-01-03 11:26:15 -0600

    Update icon and clean plugin panel class.

--- src/main/java/com/bossgear/BossGearPluginPanel.java
+++ src/main/java/com/bossgear/BossGearPluginPanel.java
@@ -8,20 +8,16 @@ import java.awt.*;
 
 public class BossGearPluginPanel extends PluginPanel {
     private static ImageIcon HELP_ICON;
-    private final JLabel mainTitle;
 
-    private static String MAIN_TITLE;
 
     static
     {
-        MAIN_TITLE = "Boss Gear";
+
     }
 
     public BossGearPluginPanel(final BossGearPlugin plugin, final ItemManager itemManager)
     {
-        this.mainTitle = new JLabel();
-        mainTitle.setText(MAIN_TITLE);
-        mainTitle.setForeground(Color.WHITE);
+
     }
 
 
